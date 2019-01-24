e34dc881b0e6a3b833d8434080bf61817554ec04
Le <hle69@vdc.csc.com>
2019-01-24 16:39:42 +0700

    HELLO

--- src/pages/ProjectsPage.js
+++ src/pages/ProjectsPage.js
@@ -1,34 +1,55 @@
 import React from 'react';
-import { Skeleton, Button } from 'antd';
+import { Skeleton, Button, Input, Icon } from 'antd';
 import { compose } from 'recompose';
 import withBreadcrumb from 'components/HOCs/withBreadcrumb';
 
 
-class PostListPage extends React.Component {
+class ProjectsPage extends React.Component {
+    constructor(props, context){
+        super(props, context);
+
+        this.copyRepoUrl = this.copyRepoUrl.bind(this);
+    }
+
+    copyRepoUrl(key){
+        if(this[`repo_url_${key}`]){
+            this[`repo_url_${key}`].input.select();
+            document.execCommand('copy');
+        }
+    }
+
     render(){
         const posts = [
             {
+                key: 'reversi',
                 title: 'Reversi',
                 level: 1/3,
                 description: 'ReactJS, Game',
+                repo_url: 'https://github.com/buyubaya/reversi',
                 demo_url: '//buyubaya.github.io/reversi'
             },
             {
+                key: 'gomoku',
                 title: 'Gomoku',
                 level: 1/3,
                 description: 'VueJS, Game',
+                repo_url: 'https://github.com/buyubaya/gomoku',
                 demo_url: '//buyubaya.github.io/gomoku'
             },
             {
+                key: 'castle_on_the_grid',
                 title: 'Castle on the Grid',
                 level: 0.4,
                 description: 'ReactJS, Algorithm',
+                repo_url: 'https://github.com/buyubaya/castle_on_the_grid',
                 demo_url: '//buyubaya.github.io/castle_on_the_grid'
             },
             {
+                key: 'ng-porfolio',
                 title: 'Book Store',
                 level: 2/3,
                 description: 'Angular, Web Application',
+                repo_url: 'https://github.com/buyubaya/ng-porfolio',
                 demo_url: '//hieu1801-ng-porfolio.herokuapp.com'
             }
         ];
@@ -37,8 +58,8 @@ class PostListPage extends React.Component {
             <section className='section'>
                 <div className='card-list'>
                     {
-                        posts.map((item, index) =>
-                            <div className='card-item' key={index}>
+                        posts.map(item =>
+                            <div className='card-item' key={item.key}>
                                 <div className='card-body'>
                                     <div className='card-info'>
                                         <h3 className='card-title'>{item.title}</h3>
@@ -49,7 +70,16 @@ class PostListPage extends React.Component {
                                         </ul>
                                         <p className='card-description'>{item.description}</p>
                                     </div>
-                                    <a href={item.demo_url} target='_blank' className='btn btn-default'>See demo</a>
+                                    <div>
+                                        <Input 
+                                            type='text' 
+                                            value={item.repo_url}  
+                                            readOnly
+                                            ref={el => this[`repo_url_${item.key}`] = el}
+                                            addonAfter={<Icon type="copy" title='Copy' onClick={() => this.copyRepoUrl(item.key)}/>}
+                                        />
+                                        <a href={item.demo_url} target='_blank' className='btn btn-default'>See demo</a>
+                                    </div>
                                 </div>
                             </div>
                         )
@@ -63,4 +93,4 @@ class PostListPage extends React.Component {
 
 export default compose(
     withBreadcrumb('postList-container')
-)(PostListPage);
+)(ProjectsPage);
