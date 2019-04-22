1e7fd57a280e43aa3e5147523e2baa3101953488
buyubaya <hihihi_caigikia@yahoo.com>
2019-04-22 23:21:50 +0700

    'HELLO'

--- src/pages/AdminPage.js
+++ src/pages/AdminPage.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
-import { Breadcrumb as AntBreadcrumb } from 'antd';
+import { Breadcrumb as AntBreadcrumb, Table } from 'antd';
 import RouterUrls from '../constants/RouterUrls';
 
 const Breadcrumb = ({ path }) => {
-    function getBreadCrumb(path, prefix='Home'){
+    function getBreadCrumb(path, prefix = 'Home') {
         let tmp = [];
 
         let currentPath = path;
-        while(currentPath !== ''){
+        while (currentPath !== '') {
             currentPath = RouterUrls.find(item => item.path === currentPath);
-            if(currentPath){
+            if (currentPath) {
                 tmp.unshift(currentPath.label);
                 currentPath = currentPath.path.split('/').slice(0, -1).join('/');
             }
@@ -24,7 +24,7 @@ const Breadcrumb = ({ path }) => {
 
     const bc = getBreadCrumb(path);
 
-    return(
+    return (
         <div className='breadcrumb-area'>
             <AntBreadcrumb>
                 {
@@ -40,10 +40,92 @@ const Breadcrumb = ({ path }) => {
 };
 
 
+const columns = [{
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+}, {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+    width: '12%',
+    render: (text, record) => <img src={'http://transcosmos.co.uk/wp-content/uploads/2015/09/google_logo_03-818x600.jpg'} className='table-image' />
+}, {
+    title: 'Address',
+    dataIndex: 'address',
+    width: '30%',
+    key: 'address',
+}];
+const data = [{
+    key: 1,
+    name: 'John Brown sr.',
+    age: 60,
+    address: 'New York No. 1 Lake Park',
+    children: [
+        {
+            key: 11,
+            name: 'John Brown',
+            age: 42,
+            address: 'New York No. 2 Lake Park',
+        },
+        {
+            key: 12,
+            name: 'John Brown jr.',
+            age: 30,
+            address: 'New York No. 3 Lake Park',
+            children: [{
+                key: 121,
+                name: 'Jimmy Brown',
+                age: 16,
+                address: 'New York No. 3 Lake Park',
+            }
+            ],
+        }, {
+            key: 13,
+            name: 'Jim Green sr.',
+            age: 72,
+            address: 'London No. 1 Lake Park',
+            children: [{
+                key: 131,
+                name: 'Jim Green',
+                age: 42,
+                address: 'London No. 2 Lake Park',
+                children: [{
+                    key: 1311,
+                    name: 'Jim Green jr.',
+                    age: 25,
+                    address: 'London No. 3 Lake Park',
+                }, {
+                    key: 1312,
+                    name: 'Jimmy Green sr.',
+                    age: 18,
+                    address: 'London No. 4 Lake Park',
+                }],
+            }],
+        }],
+}, {
+    key: 2,
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+}];
+const rowSelection = {
+    onChange: (selectedRowKeys, selectedRows) => {
+        console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
+    },
+    onSelect: (record, selected, selectedRows) => {
+        console.log(record, selected, selectedRows);
+    },
+    onSelectAll: (selected, selectedRows, changeRows) => {
+        console.log(selected, selectedRows, changeRows);
+    },
+};
+
+
 class AdminPage extends Component {
     render() {
         const { match } = this.props;
-        
+
         return (
             <div>
                 <aside className='admin-sidebar-area'>
@@ -89,6 +171,16 @@ class AdminPage extends Component {
                     {
                         match && match.path && <Breadcrumb path={match.path} />
                     }
+                    <div className='table-area'>
+                        <Table
+                            columns={columns}
+                            rowSelection={rowSelection}
+                            dataSource={data}
+                            bordered
+                            title={() => 'Header'}
+                            footer={() => 'Footer'}
+                        />
+                    </div>
                 </main>
             </div>
         )
