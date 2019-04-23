7bc2a3b492a3758c0b0feaaf7de9b854a112a604
buyubaya <hihihi_caigikia@yahoo.com>
2019-04-23 22:10:19 +0700

    'HELLO'

--- src/pages/AdminPage.js
+++ src/pages/AdminPage.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
-import { Breadcrumb as AntBreadcrumb, Table } from 'antd';
+import {
+    Breadcrumb as AntBreadcrumb,
+    Table,
+    Form, Select, InputNumber, Switch, Radio,
+    Slider, Button, Upload, Icon, Rate, Checkbox,
+    Row, Col,
+    Card
+} from 'antd';
 import RouterUrls from '../constants/RouterUrls';
 
 const Breadcrumb = ({ path }) => {
@@ -171,6 +178,7 @@ class AdminPage extends Component {
                     {
                         match && match.path && <Breadcrumb path={match.path} />
                     }
+
                     <div className='table-area'>
                         <Table
                             columns={columns}
@@ -181,11 +189,98 @@ class AdminPage extends Component {
                             footer={() => 'Footer'}
                         />
                     </div>
+
+                    <SampleForm />
                 </main>
             </div>
         )
     }
 }
+
+
+class DemoForm extends React.Component {
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+                console.log('Received values of form: ', values);
+            }
+        });
+    }
+
+    normFile = (e) => {
+        console.log('Upload event:', e);
+        if (Array.isArray(e)) {
+            return e;
+        }
+        return e && e.fileList;
+    }
+
+    render() {
+        const { getFieldDecorator } = this.props.form;
+        const formItemLayout = {
+            labelCol: { span: 6 },
+            wrapperCol: { span: 14 },
+        };
+
+        return (
+            <div className='admin-form-area'>
+                <Card loading={0}>
+                    <Form {...formItemLayout} onSubmit={this.handleSubmit}>
+                        <Form.Item
+                            label="Plain Text"
+                        >
+                            <span className="ant-form-text">China</span>
+                        </Form.Item>
+                        <Form.Item
+                            label="Select"
+                            hasFeedback
+                        >
+                            {getFieldDecorator('select', {
+                                rules: [
+                                    { required: true, message: 'Please select your country!' },
+                                ],
+                            })(
+                                <Select placeholder="Please select a country">
+                                    <Select.Option value="china">China</Select.Option>
+                                    <Select.Option value="usa">U.S.A</Select.Option>
+                                </Select>
+                            )}
+                        </Form.Item>
+                        <Form.Item
+                            label="InputNumber"
+                        >
+                            {getFieldDecorator('input-number', { initialValue: 3 })(
+                                <InputNumber min={1} max={10} />
+                            )}
+                            <span className="ant-form-text">machines</span>
+                        </Form.Item>
+                        <Form.Item
+                            label="Upload"
+                        >
+                            {getFieldDecorator('upload', {
+                                valuePropName: 'fileList',
+                                getValueFromEvent: this.normFile,
+                            })(
+                                <Upload name="logo" action="/upload.do" listType="picture">
+                                    <Button>
+                                        <Icon type="upload" /> Click to upload
+                                    </Button>
+                                </Upload>
+                            )}
+                        </Form.Item>
+                        <Form.Item
+                            wrapperCol={{ span: 12, offset: 6 }}
+                        >
+                            <Button type="primary" htmlType="submit">Submit</Button>
+                        </Form.Item>
+                    </Form>
+                </Card>
+            </div>
+        );
+    }
+}
+const SampleForm = Form.create({ name: 'validate_other' })(DemoForm);
 
 
 export default AdminPage;
