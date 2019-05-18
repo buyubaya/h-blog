067e2ea8106c419ccd28880cd81a67685806381b
buyubaya <hihihi_caigikia@yahoo.com>
2019-05-18 20:53:54 +0700

    'HELLO'

--- src/pages/ProjectsPage.js
+++ src/pages/ProjectsPage.js
@@ -15,42 +15,83 @@ class ProjectsPage extends React.Component {
         if(this[`repo_url_${key}`]){
             this[`repo_url_${key}`].input.select();
             document.execCommand('copy');
+            this[`repo_url_${key}`].input.blur();
         }
     }
 
     render(){
         const posts = [
             {
+                key: 'ng-porfolio',
+                title: 'Angular Book Store',
+                level: 2/3,
+                description: 'Angular, Web Application',
+                type: 'angular',
+                repo_url: 'https://github.com/buyubaya/ng-porfolio',
+                demo_url: '//hieu1801-ng-porfolio.herokuapp.com'
+            },
+            {
+                key: 'graphql-rx-book',
+                title: 'GraphQL-ReactJS Book Store',
+                level: 2/3,
+                description: 'ReactJS, GraphQL, Web Application',
+                type: 'reactjs',
+                repo_url: 'https://github.com/buyubaya/graphql-rx-book',
+                demo_url: 'https://buyubaya.github.io/graphql-rx-book/'
+            },
+            {
+                key: 'rx-chat',
+                title: 'ReactJS Chat',
+                level: 2/3,
+                description: 'ReactJS, GraphQL, Real-time Web Application',
+                type: 'reactjs',
+                repo_url: 'https://github.com/buyubaya/rx-chat',
+                demo_url: 'https://buyubaya.github.io/rx-chat/'
+            },
+            {
+                key: 'rx-book',
+                title: 'ReactJS Book Store',
+                level: 2/3,
+                description: 'ReactJS, Web Application',
+                type: 'reactjs',
+                repo_url: 'https://github.com/buyubaya/rx-book',
+                demo_url: 'https://buyubaya.github.io/rx-book/'
+            },
+            {
+                key: 'v-book',
+                title: 'VueJS Book Store',
+                level: 0.5,
+                description: 'VueJS, Web Application',
+                type: 'vuejs',
+                repo_url: 'https://github.com/buyubaya/v-book',
+                demo_url: 'https://buyubaya.github.io/v-book/'
+            },
+            {
                 key: 'reversi',
                 title: 'Reversi',
                 level: 1/3,
                 description: 'ReactJS, Game',
+                type: 'game',
                 repo_url: 'https://github.com/buyubaya/reversi',
-                demo_url: '//buyubaya.github.io/reversi'
+                demo_url: 'https://buyubaya.github.io/reversi'
             },
             {
                 key: 'gomoku',
                 title: 'Gomoku',
                 level: 1/3,
                 description: 'VueJS, Game',
+                type: 'game',
                 repo_url: 'https://github.com/buyubaya/gomoku',
-                demo_url: '//buyubaya.github.io/gomoku'
+                demo_url: 'https://buyubaya.github.io/gomoku'
             },
             {
                 key: 'castle_on_the_grid',
                 title: 'Castle on the Grid',
                 level: 0.4,
                 description: 'ReactJS, Algorithm',
+                type: 'algorithm',
                 repo_url: 'https://github.com/buyubaya/castle_on_the_grid',
-                demo_url: '//buyubaya.github.io/castle_on_the_grid'
-            },
-            {
-                key: 'ng-porfolio',
-                title: 'Book Store',
-                level: 2/3,
-                description: 'Angular, Web Application',
-                repo_url: 'https://github.com/buyubaya/ng-porfolio',
-                demo_url: '//hieu1801-ng-porfolio.herokuapp.com'
+                demo_url: 'https://buyubaya.github.io/castle_on_the_grid'
             }
         ];
 
@@ -60,6 +101,7 @@ class ProjectsPage extends React.Component {
                     {
                         posts.map(item =>
                             <div className='card-item' key={item.key}>
+                                <div className={`card-tag ${item.type}`}></div>
                                 <div className='card-body'>
                                     <div className='card-info'>
                                         <h3 className='card-title'>{item.title}</h3>
